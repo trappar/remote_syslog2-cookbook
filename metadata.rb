e1198c5768db3066c3f3e5f6d7e11cf550fcf7c9
Jeff Way <jway@repx.me>
2014-11-09 22:18:37 -0800

    Update license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'remote_syslog2'
 maintainer       'Jeff Way'
 maintainer_email 'jeff.way@me.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures remote_syslog2'
 long_description 'Installs/Configures remote_syslog2'
 version          '0.2.0'

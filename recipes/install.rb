10aa25d2f4bc42ece72cb15522a3c2e10a340ea5
Jeff Way <jway@repx.me>
2014-11-09 22:09:06 -0800

    Use remote_syslog2 rather than remote_syslog

--- recipes/install.rb
+++ recipes/install.rb
@@ -12,7 +12,7 @@ bash 'extract_module' do
   code <<-EOH
     mkdir -p #{install['extracted_path']}
     tar xzf #{install['download_path']} -C #{install['extract_path']}
-    mv #{install['extracted_path']}/#{install['bin']} #{bin_file}
+    mv #{install['extracted_path']}/#{install['extracted_bin']} #{bin_file}
   EOH
   not_if { ::File.exists?(bin_file) }
 end
--- test/integration/default/bats/default.bats
+++ test/integration/default/bats/default.bats
@@ -1,11 +1,11 @@
 @test "remote_syslog bin exists" {
-    which remote_syslog
+    which remote_syslog2
 }
 
 @test "remote_syslog config exists" {
-    ls /etc/remote_syslog.yml
+    ls /etc/remote_syslog2.yml
 }
 
 @test "remote_syslog is running" {
-    ls /var/run/remote_syslog.pid
+    ls /var/run/remote_syslog2.pid
 }

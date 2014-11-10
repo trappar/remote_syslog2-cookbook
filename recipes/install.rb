043b047e47e267e38e19313957109642a673f962
Jeff Way <jway@repx.me>
2014-11-10 11:23:13 -0800

    Add file cleanup to extract resource

--- recipes/install.rb
+++ recipes/install.rb
@@ -13,6 +13,7 @@ bash 'extract remote_syslog2' do
     mkdir -p #{install['extracted_path']}
     tar xzf #{install['download_path']} -C #{install['extract_path']}
     mv #{install['extracted_path']}/#{install['extracted_bin']} #{bin_file}
+    rm -rf #{install['download_path']} #{install['extracted_path']}
   EOH
   not_if { ::File.exists?(bin_file) }
 end

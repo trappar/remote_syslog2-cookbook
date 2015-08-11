68d382f7bf43a093ca2a4960dbc63aacaa356200
Kieren Evans <kevans@inviqa.com>
2015-08-11 11:44:19 +0100

    If the port is given as a string, convert it to an integer.

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -1,5 +1,12 @@
+include_recipe 'remote_syslog2::service'
+
+config = node['remote_syslog2']['config'].to_hash
+if config.key?('destination') && config['destination'].key?('port') && config['destination']['port']
+  config['destination']['port'] = config['destination']['port'].to_i
+end
+
 file node['remote_syslog2']['config_file'] do
-  content node['remote_syslog2']['config'].to_hash.to_yaml
+  content config.to_yaml
   mode '0644'
   notifies :restart, 'service[remote_syslog2]', :delayed
 end

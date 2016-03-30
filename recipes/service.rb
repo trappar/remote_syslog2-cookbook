f0a8b7c2f299d0e8079eb1cddfd3051ed8d12b6b
Rudolf Vriend <rudolfvriend@gmail.com>
2016-03-30 11:25:08 +0200

    restart also after changes to the init script content

--- recipes/service.rb
+++ recipes/service.rb
@@ -1,9 +1,11 @@
-template '/etc/init.d/remote_syslog2' do
-  source 'remote_syslog2.erb'
-  mode '0755'
-end
-
 service 'remote_syslog2' do
   supports restart: true, status: true
   action [:start, :enable]
+  end
+
+template '/etc/init.d/remote_syslog2' do
+  source 'remote_syslog2.erb'
+  mode '0755'
+  notifies :restart, 'service[remote_syslog2]', :delayed
 end
+

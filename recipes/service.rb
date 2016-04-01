c5f1483e94b884a113f90d07c1a694ef2018ee18
Rudolf Vriend <rudolfvriend@gmail.com>
2016-04-01 08:28:53 +0200

    template service before starting it

--- recipes/service.rb
+++ recipes/service.rb
@@ -1,11 +1,12 @@
-service 'remote_syslog2' do
-  supports restart: true, status: true
-  action [:start, :enable]
-  end
-
 template '/etc/init.d/remote_syslog2' do
   source 'remote_syslog2.erb'
   mode '0755'
   notifies :restart, 'service[remote_syslog2]', :delayed
 end
+
+service 'remote_syslog2' do
+  supports restart: true, status: true
+  action [:start, :enable]
+end
+
 
